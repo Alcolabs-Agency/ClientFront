e7ea2602f83a75dcb2e20f620293da984f66a7c7
Azul <111806877+azullg@users.noreply.github.com>
2024-11-12 15:51:22 -0400

    Cambio Yesenia

--- clientFront/src/pages/Home.jsx
+++ clientFront/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import ProductList from "../components/ProductList";
 import SearchBar from "../components/SearchBar";
 export default function Home() {
   // src/data/fakeDatabase.js
-
+  ///Cambio rama
   return (
     <div>
       <ProductList />
